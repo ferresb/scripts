6fed998367606bad115ec731761c26d704288b62
Bruno FERRES <ferresb@platane.home>
2023-07-10 10:27:25 +0200

    update screenlocker

--- lock.sh
+++ lock.sh
@@ -5,5 +5,6 @@ DIR=~/tmp
 rm -f $DIR/screenshot.png $DIR/screenshotblur.png
 
 scrot $DIR/screenshot.png
-convert $DIR/screenshot.png -blur 0x5 $DIR/screenshotblur.png
+convert $DIR/screenshot.png -resize 5% $DIR/screenshotblur.png
+convert $DIR/screenshotblur.png -resize 2000% $DIR/screenshotblur.png
 i3lock -i $DIR/screenshotblur.png
