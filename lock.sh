87cbcce62a928d926ae73c33741536c4b632655c
Bruno FERRES <ferresb@platane.home>
2023-07-18 10:09:23 +0200

    Update lockscreen

--- lock.sh
+++ lock.sh
@@ -6,5 +6,9 @@ rm -f $DIR/screenshot.png $DIR/screenshotblur.png
 
 scrot $DIR/screenshot.png
 convert $DIR/screenshot.png -resize 5% $DIR/screenshotblur.png
-convert $DIR/screenshotblur.png -resize 2000% $DIR/screenshotblur.png
+convert $DIR/screenshotblur.png -negate -resize 2000% $DIR/screenshotblur.png
 i3lock -i $DIR/screenshotblur.png
+
+# rm -f $DIR/screensaver.jpg
+# convert /home/ferresb/.config/awesome/themes/default/nature.jpg $DIR/screensaver.png
+# i3lock -t -i $DIR/screensaver.png
