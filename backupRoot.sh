bb3b17241753a6634ae8c02cf73ad2c5becca0f7
Bruno Ferres <bruno.ferres@univ-grenoble-alpes.fr>
2023-10-05 14:26:41 +0200

    Update script for backup

--- backupRoot.sh
+++ backupRoot.sh
@@ -7,10 +7,16 @@ WORKPATH=$ROOT/work
 TARGET=$ROOT/tmp/archive_$DATE
 TARGET_ROOT=$TARGET/root
 
-FILES=(".bash_aliases" ".bashrc" ".vimrc" ".vim" ".ssh" ".mozilla" "keepass" ".config/awesome" "Documents")
+FILES=(".bash_aliases" ".bashrc" ".vimrc" ".vim" ".ssh" "keepass" ".config/awesome" "Documents")
+
+PROGRAM_DIR=$ROOT/program
+PROGRAM_LIST=`ls $PROGRAM_DIR`
+PROGRAM_LIST_FILE=$TARGET_ROOT/program_list.txt
 
 cp -r "$WORKPATH" "$TARGET"
 mkdir $TARGET_ROOT
+
+echo $PROGRAM_LIST > $PROGRAM_LIST_FILE
 
 for ((i = 0; i < ${#FILES[@]}; i++))
 do
