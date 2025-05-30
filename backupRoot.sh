a5ee4a2ccc494dd54fd2821d231968a755000ec6
Bruno Ferres <bruno.ferres@univ-grenoble-alpes.fr>
2025-05-30 14:28:51 +0200

    Added local mails to backups

--- backupRoot.sh
+++ backupRoot.sh
@@ -7,6 +7,9 @@ WORKPATH=$ROOT/work
 TARGET=$ROOT/tmp/archive_$DATE
 TARGET_ROOT=$TARGET/root
 
+LOCAL_MAIL=$ROOT"/snap/thunderbird/common/.thunderbird/u4uxhqil.default-release/Mail/Local Folders"
+BACKUP_MAIL=$TARGET_ROOT/thunderbird_local
+
 FILES=(".bash_aliases" ".bashrc" ".vimrc" ".vim" ".gitconfig" ".ssh" "keepass" ".screenlayout" ".config/awesome" "Documents" ".tmux.conf" "Zotero")
 
 PROGRAM_DIR=$ROOT/program
@@ -22,5 +25,10 @@ for ((i = 0; i < ${#FILES[@]}; i++))
 do
     cp -r $ROOT/"${FILES[$i]}" $TARGET_ROOT
 done
+
+# backuping local mails
+mkdir $BACKUP_MAIL
+echo "$LOCAL_MAIL" > $BACKUP_MAIL/path.txt
+cp -r "$LOCAL_MAIL" "$BACKUP_MAIL/local"
 
 tar -czf $TARGET.tar.gz $TARGET
