651e8a70cd6eca25b55ed774f272a18b2594978d
Bruno Ferres <bruno.ferres@univ-grenoble-alpes.fr>
2025-10-09 13:54:06 +0200

    update backup script

--- backupRoot.sh
+++ backupRoot.sh
@@ -10,14 +10,15 @@ TARGET_ROOT=$TARGET/root
 LOCAL_MAIL=$ROOT"/snap/thunderbird/common/.thunderbird/u4uxhqil.default-release/Mail/Local Folders"
 BACKUP_MAIL=$TARGET_ROOT/thunderbird_local
 
-FILES=(".bash_aliases" ".bashrc" ".vimrc" ".vim" ".gitconfig" ".ssh" "keepass" ".screenlayout" ".config/awesome" "Documents" ".tmux.conf" "Zotero")
+FILES=(".bash_aliases" ".bashrc" ".vimrc" ".vim" ".gitconfig" ".ssh" "keepass" ".screenlayout" ".config/awesome" ".tmux.conf" "Zotero")
+DOCUMENTS="Documents"
 
 PROGRAM_DIR=$ROOT/program
 PROGRAM_LIST=`ls $PROGRAM_DIR`
 PROGRAM_LIST_FILE=$TARGET_ROOT/program_list.txt
 
 cp -r "$WORKPATH" "$TARGET"
-mkdir $TARGET_ROOT
+mkdir -p $TARGET_ROOT
 
 echo $PROGRAM_LIST > $PROGRAM_LIST_FILE
 
@@ -25,6 +26,9 @@ for ((i = 0; i < ${#FILES[@]}; i++))
 do
     cp -r $ROOT/"${FILES[$i]}" $TARGET_ROOT
 done
+
+# need a special treatment for ransomguard file...
+rsync -a --exclude '.ransomguard.pdf' $ROOT/$DOCUMENTS $TARGET_ROOT
 
 # backuping local mails
 mkdir $BACKUP_MAIL
